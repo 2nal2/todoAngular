bae59c1d1817bc8e8f65c6d4257f6bedbb888d52
dcode <donaldov7gmail.,com>
2019-03-30 15:31:07 -0600

    base de datos para produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,24 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  #config for mailing
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true # to raise error if smtp has error on setup
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      :address  => "smtp.gmail.com",
+      :port     => 587,
+      :domain   => "gmail.com",
+      # :user_name => "luisalfonsocb83@gmail.com",
+      # :password => "Lacb2208:v",
+      :user_name => "pruebadatabase@gmail.com",
+      :password => "DonaldoNexus2016!",
+      :authentication => "plain",
+      :enable_starttls_auto => true
+  }
 end
